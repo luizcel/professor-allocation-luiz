e8d03cdf7d25d32a43ad4042da3794517e599475
Luiz Oliveira <luizbarrosoliveira@gmail.com>
2022-04-05 19:50:21 -0300

    Add initial springboot Web project files

---
+++ src/main/java/com/project/professorallocation/ProfessorAllocationLuizApplication.java
@@ -0,0 +1,13 @@
+package com.project.professorallocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationLuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationLuizApplication.class, args);
+	}
+
+}
