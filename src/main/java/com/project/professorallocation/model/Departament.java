a074eed5a784a1e58f4b039b3dea63b876ee0434
Luiz Oliveira <luizbarrosoliveira@gmail.com>
2022-04-07 21:23:14 -0300

    Add departament and professor

---
+++ src/main/java/com/project/professorallocation/model/Departament.java
@@ -0,0 +1,47 @@
+package com.project.professorallocation.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "department")
+
+public class Departament {
+	@Id
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "name", length = 100, nullable = false)
+	private String name;
+	
+
+	public Departament() {
+		super();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Departament [name=" + name + ", id=" + id + "]";
+	}
+
+}
---
+++ src/main/java/com/project/professorallocation/model/Professor.java
@@ -0,0 +1,61 @@
+package com.project.professorallocation.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "professor")
+public class Professor {
+	@Id
+	@Column(nullable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private Long id;
+	
+	@Column(name = "name", length = 100, nullable = false)
+	private String name;
+	
+	@Column(length = 11, nullable = false, unique = true)
+	private String cpf;
+	
+	private Long departamentId;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Long getDepartamentId() {
+		return departamentId;
+	}
+
+	public void setDepartamentId(Long departamentId) {
+		this.departamentId = departamentId;
+	}
+
+	
+
+}
