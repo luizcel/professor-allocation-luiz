f25079e182203fe2fce3cafc4d08045356fc21c1
Luiz Oliveira <luizbarrosoliveira@gmail.com>
2022-04-12 21:36:28 -0300

    Add Professor repository List Professor findByDepartmentId

---
+++ src/main/java/com/project/professorallocation/repository/ProfessorRepository.java
@@ -0,0 +1,13 @@
+package com.project.professorallocation.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.professorallocation.model.Professor;
+
+@Repository
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+	List<Professor> findByDepartamentId(Long departmentId);
+}
