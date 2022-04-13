16f0e57add1d136dc743c906f2ff91fc4d3ee13b
Luiz Oliveira <luizbarrosoliveira@gmail.com>
2022-04-12 21:30:21 -0300

    Add department test act and print

---
+++ src/test/java/com/project/professorallocation/repository/DepartmentRepositoryTests.java
@@ -0,0 +1,32 @@
+package com.project.professorallocation.repository;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.TestPropertySource;
+
+import com.project.professorallocation.model.Department;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+@TestPropertySource(locations = "classpath:application.properties")
+public class DepartmentRepositoryTests {
+
+    @Autowired
+    DepartmentRepository departmentRepository;
+    
+    @Test
+    public void findAll() {
+        // Act
+        List<Department> department = departmentRepository.findAll();
+
+        // Print
+        department.forEach(System.out::println);
+    }
+}
