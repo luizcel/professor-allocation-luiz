4fce1790402e3d79f35d5e1ff34d5a845c2445e6
Luiz Oliveira <luizbarrosoliveira@gmail.com>
2022-04-14 21:22:56 -0300

    Create course repository 1 public void

---
+++ src/test/java/com/project/professorallocation/repository/CourseRepositoryTests.java
@@ -0,0 +1,33 @@
+package com.project.professorallocation.repository;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.TestPropertySource;
+
+import com.project.professorallocation.model.Course;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+@TestPropertySource(locations = "classpath:application.properties")
+public class CourseRepositoryTests {
+
+	@Autowired
+	private CourseRepository repository;
+	
+	@Test
+	public void findAll() {
+		List<Course> items = repository.findAll();
+		
+		System.out.println("Quantidade de elementos retornados:" + items.size());
+		for(Course item : items) {
+			System.out.println(item);
+		}
+	}
+}
