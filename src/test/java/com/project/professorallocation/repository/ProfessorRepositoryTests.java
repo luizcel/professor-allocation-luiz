b882f3f6b979e6f2975b8fb64490946cc12e747f
Luiz Oliveira <luizbarrosoliveira@gmail.com>
2022-04-14 21:22:25 -0300

    Create professor repository 1 public void

---
+++ src/test/java/com/project/professorallocation/repository/ProfessorRepositoryTests.java
@@ -0,0 +1,34 @@
+package com.project.professorallocation.repository;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.TestPropertySource;
+
+import com.project.professorallocation.model.Professor;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+@TestPropertySource(locations = "classpath:application.properties")
+public class ProfessorRepositoryTests {
+
+	@Autowired
+	private ProfessorRepository repository;
+	
+	@Test
+	public void findAll() {
+		List<Professor> items = repository.findAll();
+		
+		System.out.println("Quantidade de elementos retornados:" + items.size());
+		for(Professor item : items) {
+			System.out.println(item);
+		}
+	}
+	
+}
