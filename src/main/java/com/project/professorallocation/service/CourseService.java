df429c7f1b860e1b832482b76ad23066d44e4087
Luiz Oliveira <luizbarrosoliveira@gmail.com>
2022-04-19 17:21:11 -0300

    Create course service and methods

---
+++ src/main/java/com/project/professorallocation/service/CourseService.java
@@ -0,0 +1,51 @@
+package com.project.professorallocation.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.project.professorallocation.model.Course;
+import com.project.professorallocation.repository.CourseRepository;
+
+@Service
+public class CourseService {
+	private final CourseRepository repository;
+
+	public CourseService(CourseRepository repository) {
+		super();
+		this.repository = repository;
+	}
+
+	public Course create(Course course) {
+		course.setId(null);
+		return saveInternal(course);
+	}
+
+	public Course update(Course course) {
+		Long id = course.getId();
+		if (id == null || !repository.existsById(id)) {
+			return null;
+		} else {
+			return saveInternal(course);
+		}
+	}
+	
+	public void deleteById(Long id) {
+		if(repository.existsById(id)) {
+		repository.deleteById(id); 
+		}
+	}
+	
+	public Course findById(Long id) {
+		return repository.findById(id).orElse(null);	
+	}
+	
+	public List<Course> findAll(){
+		return repository.findAll();
+	}
+	
+	private Course saveInternal(Course course) {
+		Course createdCourse = repository.save(course);
+		return createdCourse;
+	}
+}
