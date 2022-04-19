38124d587ae8857a7b8fc6fde3db2656705460bc
Luiz Oliveira <luizbarrosoliveira@gmail.com>
2022-04-19 17:29:21 -0300

    Create professor service and methods

---
+++ src/main/java/com/project/professorallocation/service/ProfessorService.java
@@ -0,0 +1,52 @@
+package com.project.professorallocation.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.project.professorallocation.model.Professor;
+import com.project.professorallocation.repository.ProfessorRepository;
+
+@Service
+public class ProfessorService {
+	private final ProfessorRepository repository;
+
+	public ProfessorService(ProfessorRepository repository) {
+		super();
+		this.repository = repository;
+	}
+	
+	public Professor create(Professor professor) {
+		professor.setId(null);
+		return saveInternal(professor);
+	}
+
+	public Professor update(Professor professor) {
+		Long id = professor.getId();
+		if (id == null || !repository.existsById(id)) {
+			return null;
+		} else {
+			return saveInternal(professor);
+		}
+	}
+	
+	public void deleteById(Long id) {
+		if(repository.existsById(id)) {
+		repository.deleteById(id); 
+		}
+	}
+	
+	public Professor findById(Long id) {
+		return repository.findById(id).orElse(null);	
+	}
+	
+	public List<Professor> findAll(){
+		return repository.findAll();
+	}
+	
+	private Professor saveInternal(Professor professor) {
+		Professor createdProfessor = repository.save(professor);
+		return createdProfessor;
+	}
+	
+}
