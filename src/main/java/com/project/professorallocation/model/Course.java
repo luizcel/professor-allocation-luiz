006e15cc90bd51ae559765f8dbb10ffd26f9f503
Luiz Oliveira <luizbarrosoliveira@gmail.com>
2022-04-19 12:48:05 -0300

    Remove import warning not used

--- src/main/java/com/project/professorallocation/model/Course.java
+++ src/main/java/com/project/professorallocation/model/Course.java
@@ -1,6 +1,5 @@
 package com.project.professorallocation.model;
 
-import javax.annotation.Generated;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
