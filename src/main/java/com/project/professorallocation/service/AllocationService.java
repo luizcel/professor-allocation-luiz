c2dc53bb811ea7628d0870e102ed03b8cf9097f0
Luiz Oliveira <luizbarrosoliveira@gmail.com>
2022-04-20 21:00:08 -0300

    Fix logic allocation

--- src/main/java/com/project/professorallocation/service/AllocationService.java
+++ src/main/java/com/project/professorallocation/service/AllocationService.java
@@ -5,16 +5,22 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.project.professorallocation.model.Allocation;
+import com.project.professorallocation.model.Course;
+import com.project.professorallocation.model.Professor;
 import com.project.professorallocation.repository.AllocationRepository;
 
 @Service
 public class AllocationService {
 	private final AllocationRepository repository;
+    private final ProfessorService professorService;
+    private final CourseService courseService;
 
-	public AllocationService(AllocationRepository repository) {
-		super();
-		this.repository = repository;
-	}
+    public AllocationService(AllocationRepository allocationRepository, ProfessorService professorService, CourseService courseService) {
+        super();
+        this.repository = allocationRepository;
+        this.professorService = professorService;
+        this.courseService = courseService;
+    }
 
 	public Allocation findById(Long id) {
 		return repository.findById(id).orElse(null);
@@ -38,44 +44,55 @@ public class AllocationService {
 	
 	public Allocation update(Allocation allocation) {
 		Long id = allocation.getId();
-		if (id == null || !repository.existsById(id)) {
-			return null;
-		} else {
+		if (id != null && repository.existsById(id)) {
 			return saveInternal(allocation);
+		} else {
+			return null;
 		}
 	}
 
 	private Allocation saveInternal(Allocation allocation) {
-		if(hasCollision(allocation)) {
+		if(!isEndHourGreaterThanStartHour(allocation) || hasCollision(allocation)) {
 			throw new RuntimeException("There is a time collision for this allocation");
-		}
+		} else {
 		
 		allocation = repository.save(allocation);
+		
+		Professor professor = professorService.findById(allocation.getProfessorId());
+        allocation.setProfessor(professor);
+
+        Course course = courseService.findById(allocation.getCourseId());
+        allocation.setCourse(course);
+		
 		return allocation;
+		}
+	}
+
+	private boolean isEndHourGreaterThanStartHour(Allocation allocation) {
+		return allocation != null && allocation.getStartHour() != null && allocation.getEndHour() != null
+	            && allocation.getEndHour().compareTo(allocation.getStartHour()) > 0;
 	}
 
 	private boolean hasCollision(Allocation newAllocation) {
-		boolean collisionFound = false;
+		boolean hasCollision = false;
 		
 		List<Allocation> currentAllocations = repository.findByProfessorId(newAllocation.getProfessorId());
 
 		for (Allocation item : currentAllocations) {
 			if (hasCollision(item, new Allocation())) {
-				collisionFound = true;
+				hasCollision = true;
 				break;
 			}
 		}
-		return collisionFound;
+		return hasCollision;
 	}
 
 	private boolean hasCollision(Allocation currentAllocation, Allocation newAllocation) {
-		boolean collision = currentAllocation.getId().equals(newAllocation.getId())
+		return !currentAllocation.getId().equals(newAllocation.getId())
 				&& currentAllocation.getProfessorId().equals(newAllocation.getProfessorId())
-				&& currentAllocation.getDayOfWeek().equals(newAllocation.getDayOfWeek())
+				&& currentAllocation.getDayOfWeek() == newAllocation.getDayOfWeek()
 				&& currentAllocation.getStartHour().compareTo(newAllocation.getEndHour()) < 0
 				&& newAllocation.getStartHour().compareTo(currentAllocation.getEndHour()) < 0;
-
-		return collision;
 	}
 
 }
