7aa82117c0d70af24bf585267f654a6f4c919dc4
Luiz Oliveira <luizbarrosoliveira@gmail.com>
2022-04-28 19:26:10 -0300

    Add course and professor controller

--- src/main/java/com/project/professorallocation/controller/CourseController.java
+++ src/main/java/com/project/professorallocation/controller/CourseController.java
@@ -1,5 +1,59 @@
 package com.project.professorallocation.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.professorallocation.model.Course;
+import com.project.professorallocation.service.CourseService;
+
+@RestController
+@RequestMapping(path = "/courses")
 public class CourseController {
 
+	  private final CourseService service;
+
+	    public CourseController(CourseService courseService) {
+	        super();
+	        this.service = courseService;
+	    }
+	    
+	    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	    @ResponseStatus(HttpStatus.OK)
+	    public ResponseEntity<List<Course>> findALL() {
+	    	List<Course> allcourses = service.findAll(null);
+	    	return new ResponseEntity<>(allcourses, HttpStatus.OK);
+	    }
+	    
+	    @GetMapping(path = "/{course_id}",produces = MediaType.APPLICATION_JSON_VALUE)
+	    @ResponseStatus(HttpStatus.OK)
+	    public ResponseEntity<Course> findById(@PathVariable(name = "course_id") Long id) {
+	    	Course item = service.findById(id);
+	    	
+	    	if (item == null) {
+	    		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	    	}else {
+	    	return new ResponseEntity<>(item, HttpStatus.OK);
+	    }
+	    
+	}
+	    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	    @ResponseStatus(HttpStatus.CREATED)
+	    public ResponseEntity<Course> create(@RequestBody Course course){
+	    	Course item = service.create(course);
+	    	
+	    	return new ResponseEntity<>(item, HttpStatus.CREATED);
+	    }
+	    
+	    //curl -v --request POST --header "Content-Type: application/json" --header "Accept: application/json" --data-raw "{\"name\": \"BackEnd\"}" "http://localhost:8082/courses"
+	    
 }
--- src/main/java/com/project/professorallocation/controller/ProfessorController.java
+++ src/main/java/com/project/professorallocation/controller/ProfessorController.java
@@ -1,5 +1,59 @@
 package com.project.professorallocation.controller;
 
-public class ProfessorController {
+import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.professorallocation.model.Professor;
+import com.project.professorallocation.service.ProfessorService;
+
+@RestController
+@RequestMapping(path = "/professors")
+public class ProfessorController {
+	
+	private final ProfessorService service;
+
+    public ProfessorController(ProfessorService professorService) {
+        super();
+        this.service = professorService;
+    }
+    
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<List<Professor>> findALL() {
+    	List<Professor> allprofessors = service.findAll(null);
+    	return new ResponseEntity<>(allprofessors, HttpStatus.OK);
+    }
+    
+    @GetMapping(path = "/{prof_id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Professor> findById(@PathVariable(name = "prof_id") Long id) {
+    	Professor item = service.findById(id);
+    	
+    	if (item == null) {
+    		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    	}else {
+    	return new ResponseEntity<>(item, HttpStatus.OK);
+    }
+    
+}
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Professor> create(@RequestBody Professor professor){
+    	Professor item = service.create(professor);
+    	
+    	return new ResponseEntity<>(item, HttpStatus.CREATED);
+    }
+    
+    //curl -v --request POST --header "Content-Type: application/json" --header "Accept: application/json" --data-raw "{\"name\": \"Amirton\", \"cpf\": \"12345678911\", \"departmentId\": \"6\"}" "http://localhost:8082/professors"
+    
 }
