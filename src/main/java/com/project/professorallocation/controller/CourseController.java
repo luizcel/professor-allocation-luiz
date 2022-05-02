510e3d8de0636901d97baf888af47a33a7c32fc4
Luiz Oliveira <luizbarrosoliveira@gmail.com>
2022-05-02 17:45:00 -0300

    Add update and delete on CourseController

--- src/main/java/com/project/professorallocation/controller/CourseController.java
+++ src/main/java/com/project/professorallocation/controller/CourseController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -56,4 +58,27 @@ public class CourseController {
 	    
 	    //curl -v --request POST --header "Content-Type: application/json" --header "Accept: application/json" --data-raw "{\"name\": \"BackEnd\"}" "http://localhost:8082/courses"
 	    
+	    @PutMapping(path = "/{course_id}",produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	    @ResponseStatus(HttpStatus.CREATED)
+	    public ResponseEntity<Course> update(@PathVariable(name = "course_id") Long id, @RequestBody Course course){
+	    	course.setId(id);
+	    	Course item = service.update(course);
+	    	
+	    	if (item == null) {
+	    		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	    	} else {
+	    	return new ResponseEntity<>(item, HttpStatus.OK);}
+	    	}
+	    //curl -v --request PUT --header "Content-Type: application/json" --header "Accept: application/json" --data-raw "{\"name\": \"Design\"}" "http://localhost:8082/courses/3"
+	    
+	    @DeleteMapping(path = "/{course_id}")
+	    @ResponseStatus(HttpStatus.NO_CONTENT)
+	    public ResponseEntity<Void> delete(@PathVariable(name = "course_id") Long id){
+	    	
+	    	service.deleteById(id);
+	    	
+	    	return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	    }
+	    //curl -v --request DELETE "http://localhost:8082/courses/3"
 }
+
